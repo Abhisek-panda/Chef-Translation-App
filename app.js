0b0be1a87d2596886e49eb8a1d2da96a0a42aea8
Abhisek-panda <abhisekpanda789@gmail.com>
2022-09-03 15:55:51 +0530

    updated Js

---
+++ app.js
@@ -0,0 +1,5 @@
+var enterText= document.querySelector("#text-area")
+var translateBtn= document.querySelector("#btn")
+var resultText= document.querySelector("#result-text")
+
+var serverURL = "https://api.funtranslations.com/translate/chef.json";
