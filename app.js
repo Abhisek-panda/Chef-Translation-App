3d9195470e9d6962c5e64c2e9208e52913b3397d
Abhisek-panda <abhisekpanda789@gmail.com>
2022-10-03 23:48:47 +0530

    Completed the logic and CSS

--- app.js
+++ app.js
@@ -2,4 +2,44 @@ var enterText= document.querySelector("#text-area")
 var translateBtn= document.querySelector("#btn")
 var resultText= document.querySelector("#result-text")
 
-var serverURL = "https://api.funtranslations.com/translate/chef.json";
+var serverUrl = "https://api.funtranslations.com/translate/chef.json";
+
+
+function translatedUrl(text) {
+    return serverUrl + "?" + "text=" + text;
+}
+
+function userOutput(){
+    var userText = enterText.value;
+
+    fetch (translatedUrl(userText))
+    .then(response => response.json())
+    .then(json=> {
+        var newTranslationText = json.contents.translated;
+        resultText.innerText = newTranslationText;
+    })
+}
+
+
+
+
+translateBtn.addEventListener("click", userOutput)
+
+
+// function translatedURL(text) {
+//     return serverUrl + "?" + "text=" + text;
+// }
+
+// function userOutput() {
+//     var userText = inputText.value;
+
+//     fetch(translatedURL(userText))
+//     .then(response => response.json())
+//     .then(json => {
+//         var newTextTranslation = json.contents.translated;
+//         outputText.innerText = newTextTranslation;
+//     }
+//     )
+// };
+
+// inputButton.addEventListener("click", userOutput)
